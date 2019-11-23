b327c91c0f22547f1a3f642362bfb8ab63044ed2
a3y3 <sohamssd@gmail.com>
2019-11-23 12:39:07 -0500

    Add skeleton of CensusRecord [auto generated through IntelliJ]

--- src/main/java/CensusCount.java
+++ src/main/java/CensusCount.java
@@ -1,4 +1,18 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+
 public class CensusCount {
     public static void main(String[] args){
+        SparkConf sparkConf = new SparkConf();
+        sparkConf.set("spark.master", "local[4]");
+        sparkConf.setAppName("US Census Analysis");
+
+        SparkSession spark = SparkSession.builder().config(sparkConf).getOrCreate();
+        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+        jsc.close();
+        spark.close();
     }
+
+
 }
