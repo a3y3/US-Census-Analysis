5ec8c96f852aba6fb518355a423d02ce9ac49278
a3y3 <sohamssd@gmail.com>
2019-11-23 22:08:44 -0500

    Map and reduce Years column to add the values together

--- src/main/java/CensusCount.java
+++ src/main/java/CensusCount.java
@@ -1,7 +1,15 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.sql.*;
 import org.apache.spark.sql.types.DataTypes;
+import scala.*;
+
+import java.io.StringReader;
+import java.lang.Double;
 
 public class CensusCount {
     private static final String DATASET = "data.csv";
@@ -14,12 +22,12 @@ public class CensusCount {
         SparkSession sparkSession = SparkSession.builder().config(sparkConf).getOrCreate();
         JavaSparkContext jsc = new JavaSparkContext(sparkSession.sparkContext());
         CensusCount censusCount = new CensusCount();
-        censusCount.printData(sparkSession);
+        censusCount.printData(sparkSession, jsc);
         jsc.close();
         sparkSession.close();
     }
 
-    void printData(SparkSession sparkSession) {
+    void printData(SparkSession sparkSession, JavaSparkContext javaSparkContext) {
         Dataset dataset = sparkSession.read()
                 .option("header", "true")
                 .option("delimiter", ",")
@@ -62,6 +70,54 @@ public class CensusCount {
                         dataset.col("TOM_MALE").plus(dataset.col("TOM_FEMALE")));
         dataset = dataset.select("STNAME", "CTYNAME", "YEAR", "AGEGRP", "WA_TOTAL",
                 "BA_TOTAL", "IA_TOTAL", "AA_TOTAL", "NA_TOTAL", "TOM_TOTAL");
+//        dataset.show();
+//        dataset = dataset.withColumn("TOTAL_INDIVIDUALS",
+//                dataset.col("WA_TOTAL")
+//                        .plus(dataset.col("BA_TOTAL"))
+//                        .plus(dataset.col("IA_TOTAL"))
+//                        .plus(dataset.col("AA_TOTAL"))
+//                        .plus(dataset.col("NA_TOTAL"))
+//                        .plus(dataset.col("TOM_TOTAL")));
+
+        Encoder<CensusRecord> reviewEncoder = Encoders.bean(CensusRecord.class);
+        Dataset<CensusRecord> ds1 = dataset.as(reviewEncoder);
+        ds1.show();
+
+        Dataset<CensusRecord> ds2 = ds1.as(Encoders.bean(CensusRecord.class));
+        ds2.show();
+        JavaRDD<CensusRecord> javaRDD = ds2.javaRDD();
+
+
+        JavaPairRDD<String, CensusRecord> javaPairRDD = javaRDD.mapToPair(
+                (PairFunction<CensusRecord, String, CensusRecord>) censusRecord ->
+                        new Tuple2<>(censusRecord.CTYNAME, censusRecord));
+        JavaPairRDD<String, CensusRecord> result = javaPairRDD.reduceByKey((
+                Function2<CensusRecord, CensusRecord, CensusRecord>) (record1, record2) ->
+                new CensusRecord(record1.STNAME,
+                        record1.CTYNAME,
+                        record1.YEAR + record2.YEAR,
+                        record1.AGEGRP + record2.AGEGRP,
+                        record1.WA_TOTAL + record2.WA_TOTAL,
+                        record1.BA_TOTAL + record2.BA_TOTAL,
+                        record1.IA_TOTAL + record2.IA_TOTAL,
+                        record1.AA_TOTAL + record2.AA_TOTAL,
+                        record1.NA_TOTAL + record2.NA_TOTAL,
+                        record1.TOM_TOTAL + record2.TOM_TOTAL));
+
+        JavaRDD<CensusRecord> reducedValues = result.values();
+        Dataset<CensusRecord> reducedRecords = sparkSession.createDataset(reducedValues.rdd(),
+                Encoders.bean(CensusRecord.class));
+
+        dataset = reducedRecords.select("STNAME", "CTYNAME", "WA_TOTAL",
+                "BA_TOTAL", "IA_TOTAL", "AA_TOTAL", "NA_TOTAL", "TOM_TOTAL");
+        dataset = dataset.withColumn("TOTAL_INDIVIDUALS",
+                dataset.col("WA_TOTAL")
+                        .plus(dataset.col("BA_TOTAL"))
+                        .plus(dataset.col("IA_TOTAL"))
+                        .plus(dataset.col("AA_TOTAL"))
+                        .plus(dataset.col("NA_TOTAL"))
+                        .plus(dataset.col("TOM_TOTAL")));
+
         dataset.show();
     }
 }
